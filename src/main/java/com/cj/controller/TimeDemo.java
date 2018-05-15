1b4a3119be91c77f720d5f76f9201784f4b19c7b
ChenJie <New_ChenJie@163.com>
2018-05-15 16:30:28 +0800

    'f'

--- src/main/java/com/cj/controller/TimeDemo.java
+++ src/main/java/com/cj/controller/TimeDemo.java
@@ -1,45 +1,49 @@
 package com.cj.controller;
 
 
-import com.cj.domain.dto.YyDyw;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.*;
+import java.time.*;
+import java.time.temporal.IsoFields;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/time1")
 public class TimeDemo {
 
-    @GetMapping("/demo")
-    public String demo(){
-        YyDyw dyw1 = YyDyw.builder().userId(1).amountMoney(BigDecimal.valueOf(3)).build();
-        YyDyw dyw2 = YyDyw.builder().userId(1).amountMoney(BigDecimal.valueOf(2)).build();
-        YyDyw dyw3= YyDyw.builder().userId(2).amountMoney(BigDecimal.valueOf(4)).build();
-        YyDyw dyw4= YyDyw.builder().userId(2).amountMoney(BigDecimal.valueOf(4)).build();
-        List<YyDyw> dywList = new ArrayList<>();
-        dywList.add(dyw1);
-        dywList.add(dyw2);
-        dywList.add(dyw3);
-        dywList.add(dyw4);
-        Map<Integer, BigDecimal> param = new HashMap<>();
+    @GetMapping("/localtime")
+    public String localTime(@DateTimeFormat(pattern = "yyyy-MM-dd") Date time){
+        LocalDate date = LocalDate.of(1980, 10, 10);
+        long aLong = date.getLong(IsoFields.WEEK_BASED_YEAR);
+        DayOfWeek day = date.getDayOfWeek();
+        DayOfWeek saturday = DayOfWeek.SATURDAY;
+        int value = saturday.getValue();
+        System.out.println(value);
+        String name = saturday.name();
+        System.out.println(name);
+        DayOfWeek d = DayOfWeek.of(6);
+        int value1 = Month.APRIL.getValue();
+        System.out.println(value1);
+        YearMonth now = YearMonth.now();
+        System.out.println(now);
 
-        dywList.stream().map(YyDyw::getUserId).distinct().forEach(System.out::println);
-        dywList.stream().map(YyDyw::getUserId).distinct().forEach(userId ->
-        {
-            Optional<BigDecimal> reduce = dywList.stream().filter(list -> userId.equals(list.getUserId())).map(YyDyw::getAmountMoney).reduce(BigDecimal::add);
-            param.put(userId,reduce.orElse(BigDecimal.valueOf(0.00)));
-        });
-        System.out.println(param);
         return null;
     }
 
+    private void diff() {
+        LocalDate localDate = LocalDate.of(1980,10,14);
+        LocalDate date = LocalDate.of(1993,9,15);
+        Period between = Period.between(localDate, date);
+        //年,月,日
+        System.out.println(between.getYears());
+        System.out.println(between.getMonths());
+        System.out.println(between.getDays());
+        //总天数
+        System.out.println(date.toEpochDay()-localDate.toEpochDay());
+    }
 
     /**
      * localDateTime转换成Date
@@ -57,10 +61,9 @@ public class TimeDemo {
      * @param date
      * @return
      */
-    private LocalDateTime getLocalDateTime(Date date) {
+    private LocalDate getLocalDate(Date date) {
         Instant instant = date.toInstant();
-        ZoneId zoneId = ZoneId.systemDefault();
-        return instant.atZone(zoneId).toLocalDateTime();
+        return instant.atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
 
--- src/main/java/com/cj/domain/Account.java
+++ src/main/java/com/cj/domain/Account.java
@@ -1,7 +1,16 @@
 package com.cj.domain;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Account {
     private Integer id;
 
@@ -9,57 +18,11 @@ public class Account {
 
     private String phone;
 
-    private Date gmtCreate;
+    private LocalDate gmtCreate;
 
-    private Date gmtModified;
+    private LocalDateTime gmtModified;
 
     private Byte deleted;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone == null ? null : phone.trim();
-    }
-
-    public Date getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Date gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Date getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Date gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
-    public Byte getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(Byte deleted) {
-        this.deleted = deleted;
-    }
 }
