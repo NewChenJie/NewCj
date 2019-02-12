ade2332be7368545e1206bb5ae9ac683cd499200
ChenJie <New_ChenJie@163.com>
2019-02-12 10:58:31 +0800

    last

---
+++ src/main/java/com/cj/junit/AssertTest.java
@@ -0,0 +1,16 @@
+package com.cj.junit;
+
+import org.junit.Test;
+
+public class AssertTest {
+
+    @Test
+    public void testOne(){
+               if (null instanceof  Integer){
+                   System.out.println(1);
+               }
+
+    }
+
+
+}
